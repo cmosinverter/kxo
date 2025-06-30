bc7b6ffe621d2343c6f2bb51fccc15f6334d4279
kevin <kevin95120@gmail.com>
2025-06-30 16:17:54 +0800

    Refactor board display from kernel to user space
    Move the draw_board() function from kernel space to user space to
    reduce communication overhead and minimize kfifo buffer requirements.

--- xo-user.c
+++ xo-user.c
@@ -82,6 +82,28 @@ static void listen_keyboard_handler(void)
     close(attr_fd);
 }
 
+static char draw_buffer[DRAWBUFFER_SIZE];
+
+static int draw_board(char *table)
+{
+    int i = 0, k = 0;
+    draw_buffer[i++] = '\n';
+    draw_buffer[i++] = '\n';
+
+    while (i < DRAWBUFFER_SIZE) {
+        for (int j = 0; j < (BOARD_SIZE << 1) - 1 && k < N_GRIDS; j++) {
+            draw_buffer[i++] = j & 1 ? '|' : table[k++];
+        }
+        draw_buffer[i++] = '\n';
+        for (int j = 0; j < (BOARD_SIZE << 1) - 1; j++) {
+            draw_buffer[i++] = '-';
+        }
+        draw_buffer[i++] = '\n';
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (!status_check())
@@ -91,7 +113,7 @@ int main(int argc, char *argv[])
     int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 
-    char display_buf[DRAWBUFFER_SIZE];
+    char display_buf[N_GRIDS];
 
     fd_set readset;
     int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
@@ -116,9 +138,9 @@ int main(int argc, char *argv[])
         } else if (read_attr && FD_ISSET(device_fd, &readset)) {
             FD_CLR(device_fd, &readset);
             printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
-            read(device_fd, display_buf, DRAWBUFFER_SIZE);
-            display_buf[DRAWBUFFER_SIZE - 1] = '\0';
-            printf("%s", display_buf);
+            read(device_fd, display_buf, N_GRIDS);
+            draw_board(display_buf);
+            printf("%s", draw_buffer);
         }
     }
 
